eb7f52df7bdcbe5385cfcdcd0c33fb38a3182df8
Elly Loel <hello@ellyloel.com>
2026-08-08 14:32:27 +1000

    implement continuous bug spawning

--- bugs.js
+++ bugs.js
@@ -3,10 +3,17 @@ const bugs = document.querySelectorAll(`[id^="bug-"`);
 for (const bug of bugs) {
 	bug.addEventListener('click', () => {
 		bug.toggleAttribute('hidden');
+		startSpawnBugTimer();
 	});
 }
 
-window.setTimeout(() => {
+const spawnBug = () => {
 	const bug = document.querySelector(`#bug-${Math.floor(Math.random() * 7)}`);
 	bug.removeAttribute('hidden');
-}, Math.floor(Math.random() * 5000));
+};
+
+const startSpawnBugTimer = () => window.setTimeout(spawnBug, Math.floor(Math.random() * 5000));
+
+addEventListener('DOMContentLoaded', (event) => {
+	startSpawnBugTimer();
+})
